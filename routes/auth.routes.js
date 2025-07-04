4e934cd5bdc6a2b7f8d60dfdfd74f91e20a96849
N Mrithulasree <mrithula04@gmail.com>
2025-07-04 23:02:00 +0530

    swagger documentation - auth routes modification

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,7 +18,7 @@ export default authRouter;
 
 /**
  * @swagger
- * /api/v1/auth/signup:
+ * /api/v1/auth/sign-up:
  *   post:
  *     summary: Register a new user
  *     tags: [Auth]
@@ -51,7 +51,7 @@ export default authRouter;
 
 /**
  * @swagger
- * /api/v1/auth/signin:
+ * /api/v1/auth/sign-in:
  *   post:
  *     summary: Login a user
  *     tags: [Auth]
@@ -82,7 +82,7 @@ export default authRouter;
 
 /**
  * @swagger
- * /api/v1/auth/signout:
+ * /api/v1/auth/sign-out:
  *   post:
  *     summary: Logout a user (token blacklist)
  *     tags: [Auth]
